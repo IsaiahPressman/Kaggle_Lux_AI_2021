3e019beeba37f01f9defac52d8c794750743ac89
Isaiah Pressman <isaiahpressman16@gmail.com>
2021-07-13 16:09:20 -0400

    Working on creating gym-compatible Lux_AI env

---
+++ lux_ai/lux_env.py
@@ -0,0 +1,144 @@
+import atexit
+import gym
+import json
+import math
+from pathlib import Path
+import random
+from scipy.stats import rankdata
+from subprocess import Popen, PIPE
+from typing import *
+
+from .lux.game import Game
+
+DIR_PATH = Path(__file__).parent
+
+
+def cleanup_dimensions_factory(dimension_process: Popen) -> NoReturn:
+    def cleanup_dimensions():
+        if dimension_process is not None:
+            dimension_process.kill()
+    return cleanup_dimensions
+
+
+class LuxEnv(gym.Env):
+    metadata = {'render.modes': []}
+
+    def __init__(self, configuration):
+        super(LuxEnv, self).__init__()
+        # TODO: Define action and observation space using gym.spaces
+        self.action_space = None
+        self.observation_space = None
+
+        self.game_state = Game()
+        self.configuration = configuration
+        if "seed" not in self.configuration:
+            self.configuration["seed"] = math.floor(random.random() * 1e9)
+        self.done = False
+
+        # 1.1: Initialize dimensions in the background
+        self.dimension_process = Popen(
+            ["node", str(DIR_PATH / "dimensions/main.js")],
+            stdin=PIPE,
+            stdout=PIPE
+        )
+        atexit.register(cleanup_dimensions_factory(self.dimension_process))
+
+    def reset(self):
+        # 1.2: Initialize a blank state game if new episode is starting
+        self.configuration["seed"] += 1
+        initiate = {
+            "type": "start",
+            "agent_names": [],  # unsure if this is provided?
+            "config": self.configuration
+        }
+        self.dimension_process.stdin.write((json.dumps(initiate) + "\n").encode())
+        self.dimension_process.stdin.flush()
+        agent1res = json.loads(self.dimension_process.stdout.readline())
+        agent2res = json.loads(self.dimension_process.stdout.readline())
+
+        """
+        player1.observation.player = 0
+        player2.observation.player = 1
+        player1.observation.updates = agent1res
+        player2.observation.updates = agent2res
+        """
+
+        self.game_state = Game()
+        self.game_state._initialize(agent1res)
+        self.done = False
+
+        return self.obs, [0., 0.], self.done, self.info
+
+    def step(self, action):
+        # TODO: Convert actions to action list
+        # 2.: Pass in actions (json representation along with id of who made that action),
+        #       and agent information (id, status) to dimensions via stdin
+        # TODO: Construct state dictionary
+        self.dimension_process.stdin.write((json.dumps(state) + "\n").encode())
+        self.dimension_process.stdin.flush()
+
+        # 3.1 : Receive and parse the observations returned by dimensions via stdout
+        agent1res = json.loads(self.dimension_process.stdout.readline())
+        agent2res = json.loads(self.dimension_process.stdout.readline())
+        self.game_state._update(agent1res)
+
+        # Check if done
+        match_status = json.loads(self.dimension_process.stdout.readline())
+        self.done = match_status["status"] == "finished"
+
+        """
+        # 3.2: Send observations to each agent through here. 
+        #       Like dimensions, first observation can include initialization stuff, then we do the looping
+        player1.observation.updates = agent1res
+        player2.observation.updates = agent2res
+
+        player1.observation.player = 0
+        player2.observation.player = 1
+        """
+
+        # 3.3 : handle rewards when done
+        if self.done:
+            # reward here is defined as the sum of number of city tiles with unit count as a tie-breaking mechanism
+            rewards = [int(compute_reward(p)) for p in self.game_state.players]
+            rewards = (rankdata(rewards) - 1.) * 2. - 1.
+        else:
+            rewards = [0., 0.]
+
+        return self.obs, rewards, self.done, self.info
+
+    @property
+    def obs(self):
+        # TODO: Process game_state into a numpy array in some capacity?
+        # TODO: Or just leave that for a wrapper?
+        return self.game_state
+
+    @property
+    def info(self):
+        # TODO
+        return {}
+
+    def render(self, mode='human'):
+        raise NotImplementedError('LuxEnv rendering is not implemented. Use the Lux visualizer instead.')
+
+
+def interpreter(state, env):
+    player1 = state[0]
+    player2 = state[1]
+
+
+    ### 1.2: Initialize a blank state game if new episode is starting ###
+    if env.done:
+
+        return state
+
+    ##
+
+    return state
+
+
+def compute_reward(player):
+    ct_count = sum([len(v.citytiles) for k, v in player.cities.items()])
+    unit_count = len(player.units)
+    # max board size is 32 x 32 => 1024 max city tiles and units,
+    # so this should keep it strictly so we break by city tiles then unit count
+    return ct_count * 1000 + unit_count
