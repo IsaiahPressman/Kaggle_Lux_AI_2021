cb13ed0f35d1995d20efcc2e19e38df054f0179e
Isaiah Pressman <isaiahpressman16@gmail.com>
2021-09-11 19:21:25 -0400

    Added renormalization to squeeze excitation averaging. Also added dilation architecture, but it is slow

--- lux_ai/nns/__init__.py
+++ lux_ai/nns/__init__.py
@@ -4,7 +4,7 @@ from torch import nn
 from .models import BasicActorCriticNetwork
 from .in_blocks import ConvEmbeddingInputLayer
 from .attn_blocks import ViTBlock, RPSA, GPSA
-from .conv_blocks import FullConvResidualBlock
+from .conv_blocks import ResidualBlock, ParallelDilationResidualBlock
 from ..lux_gym.obs_spaces import SUBTASK_ENCODING
 from ..lux_gym.multi_subtask import MultiSubtask
 from ..utility_constants import MAX_BOARD_SIZE
@@ -21,14 +21,34 @@ def create_model(flags, device: torch.device) -> nn.Module:
                 n_merge_layers=flags.n_merge_layers,
                 use_index_select=flags.use_index_select
             ),
-            *[FullConvResidualBlock(
+            *[ResidualBlock(
                 in_channels=flags.hidden_dim,
                 out_channels=flags.hidden_dim,
                 height=MAX_BOARD_SIZE[0],
                 width=MAX_BOARD_SIZE[1],
                 kernel_size=flags.kernel_size,
                 normalize=flags.normalize,
-                activation=nn.LeakyReLU
+                activation=nn.LeakyReLU,
+                rescale_se_input=flags.rescale_se_input,
+            ) for _ in range(flags.n_blocks)]
+        )
+    elif flags.model_arch == "pd_conv_model":
+        base_model = nn.Sequential(
+            ConvEmbeddingInputLayer(
+                obs_space=obs_space.get_obs_spec(),
+                embedding_dim=flags.hidden_dim,
+                n_merge_layers=flags.n_merge_layers,
+                use_index_select=flags.use_index_select
+            ),
+            *[ParallelDilationResidualBlock(
+                in_channels=flags.hidden_dim,
+                out_channels=flags.hidden_dim,
+                height=MAX_BOARD_SIZE[0],
+                width=MAX_BOARD_SIZE[1],
+                kernel_size=flags.kernel_size,
+                normalize=flags.normalize,
+                activation=nn.LeakyReLU,
+                rescale_se_input=flags.rescale_se_input,
             ) for _ in range(flags.n_blocks)]
         )
     elif flags.model_arch == "RPSA_model":
