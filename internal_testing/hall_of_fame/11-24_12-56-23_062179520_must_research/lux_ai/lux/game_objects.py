b69c7910d8f5c0acdcdba9f70dc184559421cbc6
Isaiah Pressman <isaiahpressman16@gmail.com>
2021-12-13 12:49:56 -0500

    Added Cerberus 3.1 - final agent

---
+++ internal_testing/hall_of_fame/11-24_12-56-23_062179520_must_research/lux_ai/lux/game_objects.py
@@ -0,0 +1,200 @@
+from typing import Dict, List, Optional
+
+from .constants import Constants
+from .game_map import Position
+from .game_constants import GAME_CONSTANTS
+
+UNIT_TYPES = Constants.UNIT_TYPES
+
+
+class Player:
+    def __init__(self, team):
+        self.team = team
+        self.research_points = 0
+        self.units: List[Unit] = []
+        self.cities: Dict[str, City] = {}
+        self.city_tile_count = 0
+
+    def researched_coal(self) -> bool:
+        return self.research_points >= GAME_CONSTANTS["PARAMETERS"]["RESEARCH_REQUIREMENTS"]["COAL"]
+
+    def researched_uranium(self) -> bool:
+        return self.research_points >= GAME_CONSTANTS["PARAMETERS"]["RESEARCH_REQUIREMENTS"]["URANIUM"]
+
+    @property
+    def city_tiles(self) -> List['CityTile']:
+        return [ct for city in self.cities.values() for ct in city.citytiles]
+
+    def get_unit_by_id(self, unit_id: str) -> Optional['Unit']:
+        for unit in self.units:
+            if unit_id == unit.id:
+                return unit
+        return None
+
+
+class City:
+    def __init__(self, teamid, cityid, fuel, light_upkeep):
+        self.cityid = cityid
+        self.team = teamid
+        self.fuel = fuel
+        self.citytiles: List[CityTile] = []
+        self.light_upkeep = light_upkeep
+
+    def _add_city_tile(self, x, y, cooldown):
+        ct = CityTile(self.team, self.cityid, x, y, cooldown)
+        self.citytiles.append(ct)
+        return ct
+
+    def get_light_upkeep(self):
+        return self.light_upkeep
+
+    def __str__(self):
+        return self.cityid
+
+    def __repr__(self):
+        return f"City: {str(self)}"
+
+
+class CityTile:
+    def __init__(self, teamid, cityid, x, y, cooldown):
+        self.cityid = cityid
+        self.team = teamid
+        self.pos = Position(x, y)
+        self.cooldown = cooldown
+
+    def can_act(self) -> bool:
+        """
+        Whether or not this unit can research or build
+        """
+        return self.cooldown < 1
+
+    def research(self) -> str:
+        """
+        returns command to ask this tile to research this turn
+        """
+        return "r {} {}".format(self.pos.x, self.pos.y)
+
+    def build_worker(self) -> str:
+        """
+        returns command to ask this tile to build a worker this turn
+        """
+        return "bw {} {}".format(self.pos.x, self.pos.y)
+
+    def build_cart(self) -> str:
+        """
+        returns command to ask this tile to build a cart this turn
+        """
+        return "bc {} {}".format(self.pos.x, self.pos.y)
+
+    def __str__(self):
+        return f"CityTile: {self.pos}"
+
+    def __repr__(self):
+        return str(self)
+
+    def __hash__(self):
+        return hash(f"{self.cityid}_{self.pos}")
+
+    def __eq__(self, other):
+        return isinstance(other, CityTile) and self.pos == other.pos and self.cityid == other.cityid
+
+
+class Cargo:
+    def __init__(self):
+        self.wood = 0
+        self.coal = 0
+        self.uranium = 0
+
+    def get(self, resource_type: str) -> int:
+        if resource_type == Constants.RESOURCE_TYPES.WOOD:
+            return self.wood
+        elif resource_type == Constants.RESOURCE_TYPES.COAL:
+            return self.coal
+        elif resource_type == Constants.RESOURCE_TYPES.URANIUM:
+            return self.uranium
+        else:
+            raise ValueError(f"Unrecognized resource_type: {resource_type}")
+
+    def __str__(self) -> str:
+        return f"Cargo | Wood: {self.wood}, Coal: {self.coal}, Uranium: {self.uranium}"
+
+
+class Unit:
+    def __init__(self, teamid, u_type, unitid, x, y, cooldown, wood, coal, uranium):
+        self.pos = Position(x, y)
+        self.team = teamid
+        self.id = unitid
+        self.type = u_type
+        self.cooldown = cooldown
+        self.cargo = Cargo()
+        self.cargo.wood = wood
+        self.cargo.coal = coal
+        self.cargo.uranium = uranium
+
+    def is_worker(self) -> bool:
+        return self.type == UNIT_TYPES.WORKER
+
+    def is_cart(self) -> bool:
+        return self.type == UNIT_TYPES.CART
+
+    def get_cargo_space_left(self):
+        """
+        get cargo space left in this unit
+        """
+        spaceused = self.cargo.wood + self.cargo.coal + self.cargo.uranium
+        if self.type == UNIT_TYPES.WORKER:
+            return GAME_CONSTANTS["PARAMETERS"]["RESOURCE_CAPACITY"]["WORKER"] - spaceused
+        else:
+            return GAME_CONSTANTS["PARAMETERS"]["RESOURCE_CAPACITY"]["CART"] - spaceused
+
+    def can_build(self, game_map) -> bool:
+        """
+        whether or not the unit can build where it is right now
+        """
+        cell = game_map.get_cell_by_pos(self.pos)
+        if not cell.has_resource() and self.can_act() and (self.cargo.wood + self.cargo.coal + self.cargo.uranium) >= \
+                GAME_CONSTANTS["PARAMETERS"]["CITY_BUILD_COST"]:
+            return True
+        return False
+
+    def can_act(self) -> bool:
+        """
+        whether or not the unit can move or not. This does not check for potential collisions into other units or enemy cities
+        """
+        return self.cooldown < 1
+
+    def move(self, dir) -> str:
+        """
+        return the command to move unit in the given direction
+        """
+        return "m {} {}".format(self.id, dir)
+
+    def transfer(self, dest_id, resourceType, amount) -> str:
+        """
+        return the command to transfer a resource from a source unit to a destination unit as specified by their ids
+        """
+        return "t {} {} {} {}".format(self.id, dest_id, resourceType, amount)
+
+    def build_city(self) -> str:
+        """
+        return the command to build a city right under the worker
+        """
+        return "bcity {}".format(self.id)
+
+    def pillage(self) -> str:
+        """
+        return the command to pillage whatever is underneath the worker
+        """
+        return "p {}".format(self.id)
+
+    def __str__(self):
+        return f"Unit: team_{self.team}/type_{self.type}/pos_{self.pos}"
+
+    def __repr__(self):
+        return str(self)
+
+    def __hash__(self):
+        return hash(self.id)
+
+    def __eq__(self, other):
+        return isinstance(other, Unit) and self.id == other.id
